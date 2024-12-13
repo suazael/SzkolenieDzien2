64df8e72f2a82e32d1fce54bc271b7558f75f54d
suazael <suazael@o2.pl>
2024-12-13 13:01:28 +0100

    Add project files.

---
+++ P30AsyncAndAwait/MainWindow.xaml.cs
@@ -0,0 +1,343 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P30AsyncAndAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Thread _backgroudThread;
+
+        private void StartLongRunningTask()
+        {
+            _backgroudThread = new Thread(MonitorMemoryUsage);
+            _backgroudThread.IsBackground = true;
+            _backgroudThread.Start();
+        }
+
+        private void MonitorMemoryUsage(object? obj)
+        {
+            while (true)
+            {
+                int maxWorkerThreads, maxCompletionPortThreads;
+                int avaibleWorkerThreads, avaibleCompletionPortThreads;
+
+                ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxCompletionPortThreads);
+                ThreadPool.GetAvailableThreads(out avaibleWorkerThreads, out avaibleCompletionPortThreads);
+
+                Dispatcher.Invoke(() =>
+                {
+                    lblMonitor.Content = $"Max worker threads: {maxWorkerThreads}, Max completion port threads: {maxCompletionPortThreads}";
+                    lblMonitor.Content += $"\nAvailable worker threads: {avaibleWorkerThreads}, Available completion port threads: {avaibleCompletionPortThreads}";
+                });
+                Thread.Sleep(5000);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartLongRunningTask();
+        }
+
+        //public int CalculateValue()
+        //{
+        //    Thread.Sleep(5000);
+        //    return 100;
+        //}
+
+        public Task<int> CalculateValueAsync()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(5000);
+                return 100;
+            });
+        }
+
+        public async Task<int> CalculateValueAsync2()
+        {
+            await Task.Delay(5000);
+            return 100;
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            var n = CalculateValueAsync();
+
+            n.ContinueWith(task =>
+            {
+                lblResult.Content = task.Result.ToString();
+            },TaskScheduler.FromCurrentSynchronizationContext());
+
+
+            lblResult.Content = "Loading...";
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int value = await CalculateValueAsync2();
+            lblResult.Content = value.ToString();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using(var wc = new WebClient())
+            {
+                var result = await wc.DownloadStringTaskAsync("https://www.google.com/robots.txt");
+                lblResult.Content = result.Split("\n")[0];
+            }
+        }
+
+        private void btnGetTemperature1_Click(object sender, RoutedEventArgs e)
+        {
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+
+            foreach (var city in cities)
+            {
+                int temp = service.GetTemperature(city);
+                tbTemperature.Text += $"Temperature in {city} is currently {temp} C" + Environment.NewLine;
+            }
+        }
+
+        // secnariusz 1: jedno miasto po drugim 
+        private void btnGetTemperature2_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            foreach (var city in cities)
+            {
+                lvLogger.Items.Add($"Currently Processing {city}");
+                
+                var t = new Task<int>(() =>
+                {
+                    int temp = service.GetTemperature(city);
+                    return temp;
+                });
+                t.Start();
+
+
+               
+                tbTemperature.Text += $"Temperature in {city} is currently {t.Result} C" + Environment.NewLine;
+            }
+
+        }
+
+        // secnariusz 1: jedno miasto po drugim z uzyciem await 
+        private async void btnGetTemperature3_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            foreach (var city in cities)
+            {
+                lvLogger.Items.Add($"Currently Processing {city}");
+
+                var t = await Task.Run<int>(() => 
+                {
+                    int temp = service.GetTemperature(city);
+                    return temp;
+                });
+
+                tbTemperature.Text += $"Temperature in {city} is currently {t} C" + Environment.NewLine;
+            }
+        }
+
+        // secnariusz 1: czekamy na wszystkie zadania 
+        private async void btnGetTemperature4_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            List<Task<int>> tasks = new List<Task<int>>();
+            foreach (var city in cities)
+            {
+                var t = Task.Run<int>(() =>
+                {
+                    int temp = service.GetTemperature(city);
+                    return temp;
+                });
+                tasks.Add(t);
+            }
+
+            lvLogger.Items.Add($"Stared processing all cities");
+            await Task.WhenAll(tasks);
+            lvLogger.Items.Add($"Finished processing all cities");
+
+            foreach (var item in tasks)
+            {
+                var t = item.Result;
+                tbTemperature.Text += $"Temperature in ... is currently {t} C" + Environment.NewLine;
+            }
+          
+          
+        }
+
+        private async void btnGetTemperature5_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            List<Task> tasks = new List<Task>();
+            foreach (var city in cities)
+            {
+                var t = Task.Run(() =>
+                {
+                    int temp = service.GetTemperature(city);
+                    return (temp, city);
+                });
+                tasks.Add(t);
+            }
+
+            lvLogger.Items.Add($"Stared processing all cities");
+            await Task.WhenAll(tasks);
+            lvLogger.Items.Add($"Finished processing all cities");
+
+            foreach (Task<(int Temperture, string City)> item in tasks)
+            {
+                var t = item.Result.Temperture;
+                string cityName = item.Result.City;
+                tbTemperature.Text += $"Temperature in {cityName} is currently {t} C" + Environment.NewLine;
+            }
+
+        }
+
+        // scenariusz : nie czekamy na wszystkie zadania ale wypisujemy wnik od razu 
+        private void btnGetTemperature6_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+
+            foreach (var city in cities)
+            {
+                var t = Task.Run(() =>
+                {
+                    int temp = service.GetTemperature(city);
+                    return (temp, city);
+                });
+
+                lvLogger.Items.Add("Started processing " + city);
+
+                t.GetAwaiter().OnCompleted(() =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        tbTemperature.Text += $"Temperature in {t.Result.city} is currently {t.Result.temp} C" + Environment.NewLine;
+                    });
+                });
+            }
+        }
+
+        private async void btnGetTemperature7_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            pbProgress.Maximum = cities.Length;
+            pbProgress.Value = 0;
+
+            foreach (var city in cities)
+            {
+                lvLogger.Items.Add($"Currently Processing {city}");
+
+                var t = await Task.Run<int>(() =>
+                {
+                    int temp = service.GetTemperature(city);
+                    return temp;
+                });
+
+                tbTemperature.Text += $"Temperature in {city} is currently {t} C" + Environment.NewLine;
+                pbProgress.Value++;
+            }
+        }
+
+        private async void btnGetTemperature8_Click(object sender, RoutedEventArgs e)
+        {
+            tbTemperature.Text = string.Empty;
+            lvLogger.Items.Clear();
+            WeatherForecastService service = new WeatherForecastService();
+            string[] cities = txtCity.Text.Split(Environment.NewLine);
+            pbProgress.Maximum = cities.Length;
+            pbProgress.Value = 0;
+
+            foreach (var city in cities)
+            {
+                lvLogger.Items.Add($"Currently Processing {city}");
+
+                int temp = await service.GetTemperatureAsync3(city);
+                tbTemperature.Text += $"Temperature in {city} is currently {temp} C" + Environment.NewLine;
+                pbProgress.Value++;
+            }
+        }
+
+        private async void btnConfigureAwait_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>() { "some data", "other data", "more info" };
+
+            var processedData = await Task.Run(() =>
+            {
+                return data.Where(x=>x.Contains("data")).Select(x=>x.ToUpper()).ToList();
+            }).ConfigureAwait(true);
+
+            lblResult.Content = processedData.Count;
+        }
+
+        private async void btnConfigureAwaitDalse_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>() { "some data", "other data", "more info" };
+
+            var processedData = await Task.Run(() =>
+            {
+                return data.Where(x => x.Contains("data")).Select(x => x.ToUpper()).ToList();
+            }).ConfigureAwait(false);
+
+            await SaveToFileAsync(processedData).ConfigureAwait(false);
+        }
+
+        public async Task SaveToFileAsync(List<string> data)
+        {
+            await Task.Run(() =>
+            {
+                File.WriteAllLines("data.txt", data);
+            });
+        }
+
+        private async void btnRunTasks_Click(object sender, RoutedEventArgs e)
+        {
+            int n = 10;
+
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < n; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    Thread.Sleep(3000);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
